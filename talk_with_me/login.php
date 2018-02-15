10b24fcb2833a60c99c1e96a0f06bb9eae2bc665
Yumi-Romain <romain.boudot@epsi.fr>
2018-02-15 08:41:26 +0100

    go to responsive

--- talk_with_me/login.php
+++ talk_with_me/login.php
@@ -50,6 +50,7 @@
     <head>
 
         <title>Identifiez-vous</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
         <link rel="stylesheet" href="../../main.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
 
